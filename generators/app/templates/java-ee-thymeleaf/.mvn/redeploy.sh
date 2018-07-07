99fc85165c53fd4480aa930bc5b4e1fe0356312e
Maksim Kostromin <daggerok@gmail.com>
2018-07-07 20:39:52 +0300

    Made it to compile... (C) whatthecommit.com

--- generators/app/templates/java-ee-thymeleaf/.mvn/redeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# bash mvnw clean war:war -DskipTests; bash .mvn/redeploy.sh
-container=<%= projectDirectory %>_maven-<%= projectDirectory %>-app_1
-docker cp ./target/*.war ${container}:/home/jboss-eap-7.1/jboss-eap-7.1/standalone/deployments/app.war
-
