4f86c9a9100630a0dfebda54812cff8dfcdc5013
Maksim Kostromin <daggerok@gmail.com>
2018-07-07 16:00:16 +0300

    Merge pull my finger request (C) whatthecommit.com

--- generators/app/templates/java-ee/src/main/java/daggerok/health/HealthResource.java
+++ generators/app/templates/java-ee/src/main/java/daggerok/health/HealthResource.java
@@ -1,46 +1,58 @@
 package daggerok.health;
 
+import io.vavr.collection.HashMap;
+
 import javax.ejb.Stateless;
 import javax.json.Json;
+import javax.validation.constraints.Size;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
-@Path("api")
 @Stateless
+@Path("api")
 @Produces(APPLICATION_JSON)
 public class HealthResource {
 
-  @Context UriInfo uriInfo;
+  static final Map<String, String> pingPongMap
+    = HashMap.of("ping", "pong",
+                 "pong", "ping")
+             .toJavaMap();
+
+  @Context
+  UriInfo uriInfo;
 
   @GET
   @Path("")
-  public Response index(@Context final UriInfo uriInfo) {
+  public Response index() {
     return Response.ok(Json.createObjectBuilder()
-                           .add("_self", uriInfo.getBaseUriBuilder()
-                                                .path(HealthResource.class)
-                                                .path(HealthResource.class, "index")
-                                                .build()
-                                                .toString())
+                           .add("health", linkTo("health", "health"))
+                           .add("ping", linkTo("health", "ping"))
+                           .add("pong", linkTo("health", "pong"))
                            .build())
                    .build();
   }
 
+  String linkTo(@Size(min = 1) final String... args) {
+    return uriInfo.getBaseUriBuilder()
+                  .path(HealthResource.class)
+                  .path(HealthResource.class, args[0])
+                  .build(args[1])
+                  .toString();
+  }
+
   @GET
-  @Path("health")
-  public Response health() {
+  @Path("{path: (health|ping|pong)}")
+  public Response health(@PathParam("path") final String path) {
     return Response.ok(Json.createObjectBuilder()
-                           .add("status", "UP")
-                           .add("_self", uriInfo.getBaseUriBuilder()
-                                                .path(HealthResource.class)
-                                                .path(HealthResource.class, "health")
-                                                .build()
-                                                .toString())
+                           .add("status", pingPongMap.getOrDefault(path, "UP"))
                            .build())
                    .build();
   }
