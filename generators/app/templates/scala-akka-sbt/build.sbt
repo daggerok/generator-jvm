9649b9814e215f6a8b05209645541d69f702bd11
Maksim Kostromin <daggerok@gmail.com>
2018-07-09 00:20:50 +0300

    update scala-akka-sbt | oops, forgot to add the file (C) whatthecommit.com

--- generators/app/templates/scala-akka-sbt/build.sbt
+++ generators/app/templates/scala-akka-sbt/build.sbt
@@ -5,6 +5,7 @@ version := "0.0.1"
 scalaVersion := "2.12.6"
 
 libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.13",
   "org.scalactic" %% "scalactic" % "3.0.5",
-  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test",
 )
--- generators/app/templates/scala-akka-sbt/src/main/scala/daggerok/Application.scala
+++ generators/app/templates/scala-akka-sbt/src/main/scala/daggerok/Application.scala
@@ -1,7 +1,21 @@
 package daggerok
 
-object Application {
-  def main(args: Array[String]): Unit = {
-    println("hola!")
+import akka.actor.{Actor, ActorSystem, Props}
+
+case class MyMessage(body: String)
+
+class MyActor extends Actor {
+  override def receive: Receive = {
+    case MyMessage(body) => println(s"Hello, $body!")
   }
+}
+
+object Application extends App {
+
+  val mySystem = ActorSystem("my-actor-system")
+  val myActor = mySystem.actorOf(Props[MyActor], "my-actor")
+
+  myActor ! MyMessage("Akka")
+  Thread.sleep(1000)
+  mySystem.terminate()
 }
