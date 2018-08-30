fec4c669532d76665e5cd1a3934be54ba4286ae4
Maksim Kostromin <daggerok@gmail.com>
2018-08-30 03:06:53 +0300

    Update materializecss version up to 1.0.0-rc.2

---
+++ generators/app/templates/java-ee-thymeleaf/src/main/webapp/WEB-INF/assets/scripts-0.100.2.js
@@ -0,0 +1,49 @@
+'use strict';
+$(document).ready(function main() {
+  $('.carousel').carousel();
+  $('.collapsible').collapsible();
+  $('.tooltipped').tooltip({delay: 50});
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
+  $('.tap-target').tapTarget('close');
+  $('.materialboxed').materialbox();
+  $('#modal1').modal('close');
+  $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+  $('.scrollspy').scrollSpy();
+  $(".button-collapse").sideNav();
+  $('.collapsible').collapsible();
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true, // Choose whether you can drag to open on touch screens,
+    onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+    }
+  );
+  $('.button-collapse').sideNav('hide');
+  $('ul.tabs').tabs();
+
+});
--- generators/app/templates/java-ee-thymeleaf/src/main/webapp/WEB-INF/assets/scripts.js
+++ generators/app/templates/java-ee-thymeleaf/src/main/webapp/WEB-INF/assets/scripts.js
@@ -1,6 +1,7 @@
 (function main() {
   'use strict';
-  window.document.addEventListener('DOMContentLoaded', function onDOMContentLoaded(evt) {
-    console.log('hola!');
+  window.document.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
+    console.log('materialize initialization...');
+    M.AutoInit();
   }, false);
 })();
