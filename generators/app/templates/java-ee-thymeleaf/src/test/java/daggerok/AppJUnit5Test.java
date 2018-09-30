6b4c92b782dec52e88b9af8ca97ee4c20956b7b1
Maksim Kostromin <daggerok@gmail.com>
2018-09-30 16:40:16 +0300

    Add junit-platform.properties file into src/test/resources directory.

--- generators/app/templates/java-ee-thymeleaf/src/test/java/daggerok/AppJUnit5Test.java
+++ generators/app/templates/java-ee-thymeleaf/src/test/java/daggerok/AppJUnit5Test.java
@@ -1,34 +1,18 @@
 package daggerok;
 
-import daggerok.extensions.CaptureSystemOutput;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Slf4j
-@CaptureSystemOutput
 @DisplayName("JUnit 5 modern tests")
 class AppJUnit5Test {
 
   @Test
-  void test(final CaptureSystemOutput.OutputCapture outputCapture) {
-
-    outputCapture.expect((containsString("'assert before'")));
-    log.info("see line 20: we are expecting this string: 'assert before'");
-
-    log.info("see next line 20: 'assert after'");
-    assertThat(outputCapture.toString(), containsString("'assert after'"));
-  }
-
-  @Test
   @DisplayName("Positive test")
   void testPositive() {
-    assertThat(true, is(true));
+    assertThat(true).isTrue();
   }
 
   @Test
@@ -36,7 +20,7 @@ class AppJUnit5Test {
   void testNegative() {
 
     assertThrows(AssertionError.class,
-                 () -> assertThat(false, is(true)),
+                 () -> assertThat(false).isTrue(),
                  "Assertion error should thrown");
   }
 }
