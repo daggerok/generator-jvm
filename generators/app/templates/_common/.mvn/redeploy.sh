0b702357042a1258f5ab0099872e598e9cc61d9e
Maksim Kostromin <daggerok@gmail.com>
2018-11-25 17:37:20 +0200

    Gradle 5.0 RC 5.

--- generators/app/templates/_common/.mvn/redeploy.sh
+++ generators/app/templates/_common/.mvn/redeploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # bash mvnw clean war:war -DskipTests; bash .mvn/redeploy.sh
-container=<%= projectDirectory %>_maven-<%= projectDirectory %>-app_1
+container=<%= projectDirectory %>_<%= projectDirectory %>_1
 docker cp ./target/*.war ${container}:/home/jboss-eap-7.1/jboss-eap-7.1/standalone/deployments/app.war
 
